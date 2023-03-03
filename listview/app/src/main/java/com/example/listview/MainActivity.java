3919dba27ee6f2b2fbd402501fb8e26529a3d784
mahmud-bsr <80294885+mahmud-bsr@users.noreply.github.com>
2023-03-03 15:07:38 +0700

    listview

---
+++ listview/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listview);
+        adapter = ArrayAdapter.createFromResource(this,R.array.nama_negara,android.R.layo);
+    }
+}
